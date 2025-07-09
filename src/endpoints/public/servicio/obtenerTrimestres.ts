13890c6bc21fe64feedbaf8b79f3a65d2137156f
KillMidory <mariana.iniguez9142@alumnos.udg.mx>
2025-07-08 22:29:13 -0600

    [API](fix)-> se solucionó un problema en las fechas de los semestres

--- src/endpoints/public/servicio/obtenerTrimestres.ts
+++ src/endpoints/public/servicio/obtenerTrimestres.ts
@@ -10,22 +10,11 @@ export default async function obtenerCompleto(req: SolicitudPersonalizada, res: 
     if (!generales) {
       return res.status(404).send({ code: 'SERVICIO_NO_ENCONTRADO' });
     }
-
-    // Determinamos si el usuario es nuevo o viejo según la fecha de inicio de su servicio
-    const esUsuarioNuevo = new Date(generales.fechaInicio) > new Date('2025-01-01');
-
+    
     const trimestres: Trimestre[] = await baseDatos.almacenamientoTrimestre
       .obtenerPorFechas(generales.fechaInicio, generales.fechaFin);
 
-    // Si es un usuario nuevo, solo devolver los semestres
-    const trimestresFiltrados = esUsuarioNuevo
-      ? trimestres.filter(trimestre => {
-          const mes = new Date(trimestre.fechaInicio).getMonth();  
-          return [2, 8].includes(mes);  // Semestres: Febrero (2) y Agosto (8)
-        })
-      : trimestres;
-
-    return res.status(200).send(trimestresFiltrados);
+    return res.status(200).send(trimestres);
   } catch (err) {
     return res.status(500).send({ code: 'ERROR_DE_BASE_DE_DATOS' });
   }
